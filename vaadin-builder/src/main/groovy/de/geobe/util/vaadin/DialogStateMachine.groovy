a318a95b5ea785dc18a4f2e529bbefcabc4ab279
geobe <code@georgbeier.de>
2016-01-14 10:56:34 +0100

    some comment cosmetics, no functional changes

--- vaadin-builder/src/main/groovy/de/geobe/util/vaadin/DialogStateMachine.groovy
+++ vaadin-builder/src/main/groovy/de/geobe/util/vaadin/DialogStateMachine.groovy
@@ -3,7 +3,7 @@ package de.geobe.util.vaadin
 import groovy.util.logging.Slf4j
 
 /**
- * Implementation of a simple Moore state machine
+ * Implementation of a simple state machine
  * Created by georg beier on 01.12.2015.
  */
 @Slf4j
@@ -31,8 +31,8 @@ class DialogStateMachine<S extends Enum, E extends Enum> {
     /**
      * add an action to this state machine
      * @param state the current state
-     * @param nextState the state to go after executing the action. Can be overwritten by the action
-     *                  if it is returning an Enum<S> value
+     * @param nextState the state to go after executing the action.
+     *        Can be overwritten by the action if it is returning an Enum<S> value
      * @param event event that triggers the state machine
      * @param action closure that is executed for combination of state and event
      */
@@ -54,11 +54,14 @@ class DialogStateMachine<S extends Enum, E extends Enum> {
     }
 
     /**
-     * execute closure that is identified by currentState and event. After execution, statemachine will be
-     * <ul><li>either in the following state as defined in addAction method, if closure returns no object of type S</li>
+     * execute closure that is identified by currentState and event.
+     * After execution, statemachine will be
+     * <ul><li>either in the following state as defined in addAction method,
+     * if closure returns no object of type S</li>
      * <li>or in the state returned by the closure.</li></ul>
      * @param event triggering event
-     * @param params optional parameter to closure. Caution, closure will receive an Object[] Array
+     * @param params optional parameter to closure.
+     *        Caution, closure will receive an Object[] Array
      * @return the current state after execution
      */
     S execute(E event, Serializable... params) {
